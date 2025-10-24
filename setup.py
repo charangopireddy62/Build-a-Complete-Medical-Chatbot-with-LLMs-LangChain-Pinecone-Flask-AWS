7fc26d5972bea8e56bb3f62b6305caaab97701a9
Charan Gopireddy <charangopireddy62@gmail.com>
2025-10-24 19:29:38 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="sri charan gopireddy",
+    author_email="charangopireddy62@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    )
